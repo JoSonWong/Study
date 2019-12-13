d42848887710f228cba06384743c228d82baa86e
JoSonWong <81642606@qq.com>
2019-12-13 23:00:12 +0800

    格式化代码

--- education/src/main/java/com/jwong/education/ui/clock/ClockFragment.java
+++ education/src/main/java/com/jwong/education/ui/clock/ClockFragment.java
@@ -4,10 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -34,7 +33,7 @@ public class ClockFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.btn_clock:
 
                 break;
